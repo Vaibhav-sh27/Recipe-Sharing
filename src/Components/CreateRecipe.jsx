d64123af54eb86d338ea3587fc2945355464bef5
Vaibhav-sh27 <Vaibhavshrotriyas@gmail.com>
2024-06-15 15:13:06 +0530

    create

---
+++ src/Components/CreateRecipe.jsx
@@ -0,0 +1,82 @@
+import React, { useContext, useRef } from 'react'
+import { Context } from '../contexts/Context';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {
+    MDBBtn,
+    MDBModal,
+    MDBModalDialog,
+    MDBModalContent,
+    MDBModalHeader,
+    MDBModalTitle,
+    MDBModalBody,
+    MDBModalFooter,
+    MDBInput,
+  } from 'mdb-react-ui-kit';
+import axios from 'axios';
+import { useAuth } from '../contexts/AuthContext';
+
+const CreateRecipe = () => {
+    const {show, setShow, play, setPlay,showAdd, setShowAdd} = useContext(Context);
+    const {currUser,setUser} = useAuth();
+    let name = useRef();
+    let access= useRef();
+    const handleClose = () => setShowAdd(false);
+
+    async function handleCreate() {
+      if(name.current.value.trim()){
+        let playObj= {
+          name: name.current.value,
+          access: access.current.value,
+          owner: currUser.email,
+          movies:[],
+      }
+      let res = await axios.post(`${import.meta.env.VITE_BAPI_URL}/playlist/${currUser._id}/add`, playObj)
+      console.log(res.data);
+      setPlay((item)=>[...item, res.data.data.playlistData]);
+      setUser(res.data.data.currUser);
+      }
+        // setPlay((item)=>[...item, playObj]);
+    }
+  return (
+    <div>
+        <MDBModal open={showAdd} onClose={handleClose} tabIndex='-1' >
+        <MDBModalDialog >
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle >Create Recipe</MDBModalTitle>
+            </MDBModalHeader>
+            <MDBModalBody>
+            <MDBInput label="PlayList Name" id="typeText" type="text" ref={name} />
+            <div className='mb-2' style={{display:"flex", justifyContent:'space-between', padding:'5px 5px', marginTop:'10px'}}>
+          <label htmlFor="gender" style={{fontSize:'20px'}}>Access</label>
+          <select style={{width:"70%"}} data-mdb-select-init ref={access}>
+            <option value="Public">Public</option>
+            <option value="Private">Private</option>
+          </select>
+          </div>
+            </MDBModalBody>
+
+            <MDBModalFooter>
+            <MDBBtn className='mb-3'  color='success' onClick={()=>{
+                handleCreate();
+                handleClose();
+              }}>
+                Create
+              </MDBBtn>
+            <MDBBtn className='mb-3' onClick={()=>{
+                handleClose();
+              }}>
+                Close
+              </MDBBtn>
+
+
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+    </div>
+  )
+}
+
+export default CreateRecipe
